bb7907aeb634a08eb90b70b3c02c28a7e3e58e4d
Adam Onishi <onishiweb@gmail.com>
2016-11-24 10:11:54 +0000

    Update comments to show correct allowed values

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function(config) {
 
 
 		// test results reporter to use
-		// possible values: 'dots', 'progress', 'istanbul'
+		// possible values: 'dots', 'progress', 'coverage'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
 		reporters: ['progress', 'coverage'],
 
