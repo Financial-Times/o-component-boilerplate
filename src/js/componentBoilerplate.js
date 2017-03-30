0fc4bebaeb8daa6e0e200fe0e8d4d764314aa65d
Adam Onishi <onishiweb@gmail.com>
2017-03-30 11:37:24 +0100

    Update the init task to use some handy ES2015

--- src/js/componentBoilerplate.js
+++ src/js/componentBoilerplate.js
@@ -12,10 +12,10 @@ class ComponentBoilerplate {
 		if (!(rootEl instanceof HTMLElement)) {
 			rootEl = document.querySelector(rootEl);
 		}
-		if (rootEl instanceof HTMLElement && /\bo-component-boilerplate\b/.test(rootEl.getAttribute('data-o-component'))) {
+		if (rootEl instanceof HTMLElement && rootEl.matches('[data-o-component=o-component-boilerplate]')) {
 			return new ComponentBoilerplate(rootEl, opts);
 		}
-		return [].map.call(rootEl.querySelectorAll('[data-o-component="o-component-boilerplate"]'), rootEl => new ComponentBoilerplate(rootEl, opts));
+		return Array.from(rootEl.querySelectorAll('[data-o-component="o-component-boilerplate"]'), rootEl => new ComponentBoilerplate(rootEl, opts));
 	}
 }
 
